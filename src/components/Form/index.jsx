5a1cb0adf953809a0adc69cae3e57bca26d8cc01
Abubakr <thefox5000@gmail.com>
2023-11-03 07:36:29 +0300

    Add password error msg

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,50 +1,87 @@
 import { useState } from "react";
-import './style.css'
+import "./style.css";
 
+import PasswordMsg from "../PasswordMsg";
 
 export default function Form() {
-
+  const [password, setPassword] = useState("");
+  const [togglePasswordMsg, setTogglePasswordMsg] = useState(false);
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const handleBlur = (e) => {
+    console.log(e.target.value);
+    setTogglePasswordMsg(!togglePasswordMsg);
+  }
   return (
     <>
       <div className="App">
-        <form onSubmit={handleSubmit}>
+        <form>
           <fieldset>
             <h2>Sign Up</h2>
             <div className="Field">
               <label htmlFor="firstname">
                 First name <sup>*</sup>
               </label>
-              <input id="firstname" placeholder="First name" />
+              <input
+                value={firstname}
+                onChange={(e) => setFirstname(e.target.value)}
+                id="firstname"
+                placeholder="First name"
+              />
             </div>
             <div className="Field">
-              <label>Last name</label>
-              <input placeholder="Last name" />
+              <label htmlFor="lastname">Last name</label>
+              <input
+                value={lastname}
+                onChange={(e) => setLastname(e.target.value)}
+                id="lastname"
+                placeholder="Last name"
+              />
             </div>
             <div className="Field">
-              <label>
+              <label htmlFor="email">
                 Email address <sup>*</sup>
               </label>
-              <input placeholder="Email address" />
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                id="email"
+                placeholder="Email address"
+              />
             </div>
             <div className="Field">
-              <label>
+              <label htmlFor="password">
                 Password <sup>*</sup>
               </label>
-              <input placeholder="Password" />
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                onBlur={e => e.target.value.length > 8? null: () => handleBlur}
+                id="password"
+                placeholder="Password"
+              />
+              {togglePasswordMsg && <PasswordMsg />}
             </div>
             <div className="Field">
-              <label>
+              <label htmlFor="role">
                 Role <sup>*</sup>
               </label>
-              <select>
+              <select
+                value={role}
+                onChange={(e) => setRole(e.target.value)}
+                id="role"
+              >
                 <option value="role">Role</option>
                 <option value="individual">Individual</option>
                 <option value="business">Business</option>
               </select>
             </div>
-            <button type="submit" disabled={!getIsFormValid()}>
+            <button type="submit">
               Create account
             </button>
+            {/* {disabled={!getIsFormValid()}} */}
           </fieldset>
         </form>
       </div>
---
+++ src/components/PasswordMsg/index.jsx
@@ -0,0 +1,7 @@
+import './style.css';
+
+export default function PasswordMsg () {
+    return (
+        <p>Password should be at least 8 charachters!</p>
+    )
+}
