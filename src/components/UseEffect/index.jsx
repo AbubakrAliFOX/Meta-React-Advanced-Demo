05991d15a56bcf5aeb32f6e431756442cb9793f8
Abubakr <thefox5000@gmail.com>
2023-11-05 08:28:57 +0300

    Display customer data

--- src/components/UseEffect/index.jsx
+++ src/components/UseEffect/index.jsx
@@ -8,5 +8,13 @@ export default function Loading() {
       .then(response => setUser(response.results[0]))
       .catch(err => console.log(err));
   }
-  return <h1>Pending ...</h1>
+  useEffect(() => {
+    fetchData();
+  }, [])
+  return <div>
+    <h1>Pending ...</h1>
+    <h1>Customer Data</h1>
+    <h2>{user.name? user.name.first: 'Person' } {user.name? user.name.last: 'Person' }</h2>
+    <img src={user.picture? user.picture.large: ''} alt="Customer Photo" height={250} width={200} />
+  </div>
 }
