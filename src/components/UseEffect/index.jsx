c53903bc8372276e3d0415bcaf4517fcb558a940
Abubakr <thefox5000@gmail.com>
2023-11-05 08:38:47 +0300

    Add loading Gif

--- src/components/UseEffect/index.jsx
+++ src/components/UseEffect/index.jsx
@@ -4,17 +4,28 @@ export default function Loading() {
   const [user, setUser] = useState({});
   const fetchData = () => {
     fetch(`https://randomuser.me/api/?results=1`)
-      .then(data => data.json())
-      .then(response => setUser(response.results[0]))
-      .catch(err => console.log(err));
-  }
+      .then((data) => data.json())
+      .then((response) => setUser(response.results[0]))
+      .catch((err) => console.log(err));
+  };
   useEffect(() => {
     fetchData();
-  }, [])
-  return <div>
-    <h1>Pending ...</h1>
-    <h1>Customer Data</h1>
-    <h2>{user.name? user.name.first: 'Person' } {user.name? user.name.last: 'Person' }</h2>
-    <img src={user.picture? user.picture.large: ''} alt="Customer Photo" height={250} width={200} />
-  </div>
+  }, []);
+  return Object.keys(user).length > 0 ? (
+    <div>
+      <h2>Customer Data</h2>
+      <h3>
+        {user.name ? user.name.first : "Person"}{" "}
+        {user.name ? user.name.last : "Person"}
+      </h3>
+      <img
+        src={user.picture ? user.picture.large : ""}
+        alt="Customer Photo"
+        height={250}
+        width={200}
+      />
+    </div>
+  ) : (
+    <img src="1488.gif" alt="Pending GIF" />
+  );
 }
