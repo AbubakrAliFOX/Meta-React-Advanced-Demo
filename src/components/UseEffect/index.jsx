555d313555c0d289f7aa161ec79540984d318afe
Abubakr <thefox5000@gmail.com>
2023-11-05 08:20:57 +0300

    Create fetchData function

--- src/components/UseEffect/index.jsx
+++ src/components/UseEffect/index.jsx
@@ -1,6 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Loading() {
   const [user, setUser] = useState({});
+  const fetchData = () => {
+    fetch(`https://randomuser.me/api/?results=1`)
+      .then(data => data.json())
+      .then(response => setUser(response.results[0]))
+      .catch(err => console.log(err));
+  }
   return <h1>Pending ...</h1>
 }
